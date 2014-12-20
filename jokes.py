539a060a0fe174e99f127e4f6290845101f9d683
Technoboy10 <technoboy10@users.noreply.github.com>
2014-12-19 19:37:49 -0700

    Ahahaha calculus jokes

--- jokes.py
+++ jokes.py
@@ -1,5 +1,6 @@
 jokes = [
     "You kill vegetarian vampires with a steak to the heart.", 
     "Is it solipsistic in here, or is it just me?",            
-    "What do you call two crows on a branch? Attempted murder."
+    "What do you call two crows on a branch? Attempted murder.",
+    "Q: Why did the chicken cross the road? A: The answer is trivial, and is left as an exercise for the reader."
 ]
